e9a6e785095a442e43969f668d614297aa42d389
swatiamberkar <swatiamberkar29@gmail.com>
2019-01-15 15:23:48 +0530

    Test

---
+++ src/example/Basic.java
@@ -0,0 +1,19 @@
+package example;
+
+public class Basic {
+
+
+		public static void main(String[] args) {
+		String dateTimeText ="Time 13 : 11";
+
+		String dateValue = dateTimeText.split(" ")[1];
+		System.out.println("dateValue: "+dateValue);
+		
+		String dateValue1 = dateTimeText.split(" ")[3];
+		System.out.println("dateValue: "+dateValue1);
+
+		  
+		}
+
+		}
+
